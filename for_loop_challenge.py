bd035461695c60e56fe17f42ddc94a8cafc8bd7e
Omar El Sayeed <90359498+matrixjpg@users.noreply.github.com>
2021-09-09 08:47:34 -0500

    Add files via upload

---
+++ for_loop_challenge.py
@@ -0,0 +1,16 @@
+# Assign An Array With Number Of Different Fruits
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+
+# Just Print Out This String
+print("Our fruit selection:")
+
+# Creates A For Loop Stores Every Element in Variable fruit and prints it out
+for fruit in fruits:
+    print(fruit)
